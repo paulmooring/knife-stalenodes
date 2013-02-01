b8da8806ebe94a745714f3cde2c490a468a2c32b
Paul Mooring <paul@opscode.com>
2013-01-31 20:38:51 -0700

    Got plugin working

--- lib/chef/knife/stalenodes.rb
+++ lib/chef/knife/stalenodes.rb
@@ -16,8 +16,8 @@
 #
 
 module KnifeSuppport
-  class Support < Chef::Knife
-    banner "knife support [HOST] (options)"
+  class Stalenodes < Chef::Knife
+    banner "knife stalenodes [options]"
 
     # Don't lazy load or you'll get an error
     require 'chef/environment'
@@ -26,140 +26,82 @@ module KnifeSuppport
     require 'chef/api_client'
     # Do lazy load this stuff
     deps do
-      require 'chef/knife/ssh'
+      require 'highline'
+      require 'chef/search/query'
       require 'chef/mixin/command'
-      require 'net/ssh'
-      require 'net/ssh/multi'
     end
 
-    [:knife, :nodes, :roles, :environments, :clients, :databags].each do |opt|
-      option opt,
-          :short => "-#{opt.to_s[0]}",
-          :long => "--#{opt.to_s}",
-          :boolean => true,
-          :description => "Include details on #{opt.to_s}"
+    option :reverse,
+        :short        => "-r",
+        :long         => "--reverse",
+        :description  => "Reverses the search to return only nodes that have checked in recently",
+        :boolean      => true
+
+    option :days,
+        :short        => "-D DAYS",
+        :long         => "--days DAYS",
+        :description  => "The days since last check in",
+        :default      => 0
+
+    option :hours,
+        :short        => "-H HOURS",
+        :long         => "--hours HOURS",
+        :description  => "Adds hours to days since last check in",
+        :default      => 1
+
+    option :minutes,
+        :short        => "-m MINUTES",
+        :long         => "--minutes MINUTES",
+        :description  => "Adds minutes to hours since last check in",
+        :default      => 0
+
+
+    def calculate_time
+      seconds = config[:days].to_i * 86400 + config[:hours].to_i * 3600 + config[:minutes].to_i * 60
+
+      return Time.now.to_i - seconds
     end
 
-    option :all,
-        :short => '-a',
-        :long => '--all',
-        :boolean => true,
-        :description => "Include everything (except data bags)"
-
-    option :really_all,
-        :short => '-A',
-        :long => '--really-all',
-        :boolean => true,
-        :description => "Include everything (including data bags)"
-
-    option :ssh_user,
-        :short => "-x USERNAME",
-        :long => "--ssh-user USERNAME",
-        :description => "The ssh username"
-
-    option :ssh_password,
-        :short => "-P PASSWORD",
-        :long => "--ssh-password PASSWORD",
-        :description => "The ssh password"
-
-    option :ssh_port,
-        :short => "-p PORT",
-        :long => "--ssh-port PORT",
-        :description => "The ssh port",
-        :default => "22",
-        :proc => Proc.new { |key| Chef::Config[:knife][:ssh_port] = key }
-
-    option :connect_attribute,
-        :short => "-C ATTR",
-        :long => "--connect-attribute ATTR",
-        :description => "The attribute to use for opening the connection - default is fqdn",
-        :default => "fqdn"
-
-    option :use_sudo,
-        :long => "--sudo",
-        :description => "Execute the bootstrap via sudo",
-        :boolean => true
-
-    option :host_key_verify,
-        :long => "--[no-]host-key-verify",
-        :description => "Verify host key, enabled by default.",
-        :boolean => true,
-        :default => true
-
-    [Chef::Environment, Chef::Role, Chef::Node, Chef::ApiClient, Chef::DataBag].each do |klass|
-      component = klass.to_s.downcase.gsub("chef::", "").concat('s')
-      define_method "get_#{component}".to_sym do
-        msg "==== #{component} ===="
-        klass.list.each do |name, uri|
-          msg "=== #{name} ===" 
-          output format_for_display(klass.load(name))
-          msg "=== End #{name} ===" 
-        end
-        msg "==== End #{component} ===="
-      end
-    end
-
-    def knife_ssh(host)
-      command = if config[:use_sudo]
-        "sudo chef-client -l debug"
+    def get_query
+      if config[:reverse]
+        "ohai_time:[#{calculate_time} TO *]"
       else
-        "chef-client -l debug"
-      end
-
-      ssh = Chef::Knife::Ssh.new
-      ssh.ui = ui
-      ssh.name_args = [ host, command ]
-      ssh.config[:ssh_user] = config[:ssh_user]
-      ssh.config[:ssh_user] ||= 'root'
-      ssh.config[:ssh_password] = config[:ssh_password]
-      ssh.config[:ssh_port] = Chef::Config[:knife][:ssh_port] || config[:ssh_port]
-      ssh.config[:identity_file] = config[:identity_file]
-      ssh.config[:manual] = true
-      ssh.config[:host_key_verify] = config[:host_key_verify]
-      ssh.config[:on_error] = :raise
-      ssh.config[:attribute] = config[:connect_attribute]
-      begin
-        ssh.run
-      rescue Net::SSH::AuthenticationFailed
-        if ssh.config[:ssh_password].nil?
-          msg "Failed to authenticate #{config[:ssh_user]} - trying password auth"
-          config[:ssh_password] ||= ui.ask("Enter your password: ") { |q| q.echo = false }
-          knife_ssh(host)
-        end
+        "ohai_time:[* TO #{calculate_time}]"
       end
     end
 
-    def get_config
-      msg "==== configuration ===="
-      msg "=== config ==="
-      pp config
-      msg "=== End config ==="
-      msg "=== Chef::Config ==="
-      pp Chef::Config.configuration
-      msg "=== End Chef::Config ==="
-      msg "==== End configuration ===="
+    def check_last_run_time(time)
+      diff = Time.now.to_i - time
+      minutes = (diff / 60)
+      days = (minutes / 60 / 24)
+
+      case
+      when diff < 3600
+          {
+            :color => :green,
+            :text => "#{minutes} minute#{minutes == 1 ? ' ' : 's'}"
+          }
+      when diff > 86400
+          {
+            :color => :red,
+            :text => "#{days} day#{days == 1 ? ' ' : 's'}"
+          }
+      else
+          {
+            :color => :yellow,
+            :text => "#{minutes / 60} hours"
+          }
+      end
     end
 
     def run
-      unless config[:knife] or config[:nodes] or config[:roles] or config[:environments] or config[:clients] or config[:databags]
-        config[:all] = true
-      end
-      if config[:really_all]
-        config[:all] = true
-        warn "The --really-all will include your data bags and data contained in them."
-        confirm "Do you want to continue"
-      elsif config[:databags]
-        warn "The --databags will include your data bags and data contained in them."
-        confirm "Do you want to continue"
-      end
+      query = Chef::Search::Query.new
 
-      get_config if config[:knife] or config[:all]
-      get_nodes if config[:nodes] or config[:all]
-      get_roles if config[:roles] or config[:all]
-      get_environments if config[:environments] or config[:all]
-      get_apiclients if config[:clients] or config[:all]
-      get_databags if config[:databags] or config[:really_all]
-      knife_ssh name_args.first if name_args.first
+      query.search(:node, get_query).first.each do |node|
+        # nodes.each do |node|
+        msg = check_last_run_time(node['ohai_time'].to_i)
+        HighLine.new.say "#{@ui.color(msg[:text], msg[:color])} ago: #{node.name}"
+      end
     end
   end
 end
